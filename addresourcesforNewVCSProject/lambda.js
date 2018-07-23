2c80626f73d3de192bc21d3f59c83edc8ad91457
TestAndunR <andunranmal@gmail.com>
2018-07-23 09:54:51 +0530

    add a twitter

--- addresourcesforNewVCSProject/lambda.js
+++ addresourcesforNewVCSProject/lambda.js
@@ -1,18 +1,29 @@
 let AWS = require('aws-sdk');
+let SL_TWITTER = require('slappforge-sdk-twitter');
+let twitterClients = require('./TwitterClients');
+const twitter = new SL_TWITTER.TwitterP(twitterClients);
 let SL_AWS = require('slappforge-sdk-aws');
 let connectionManager = require('./ConnectionManager');
 const rds = new SL_AWS.RDS(connectionManager);
 
-exports.handler = function(event, context, callback) {
- 
+exports.handler = function (event, context, callback) {
+
     // You must always end/destroy the DB connection after it's used
     rds.beginTransaction({
         instanceIdentifier: 'TestDB'
-    },function (error,connection) {
+    }, function (error, connection) {
         if (error) {
             throw error;
         }
     });
-    
-    callback(null,'Successfully executed');
+    twitter.postTweet({
+        "status": "Helloooo",
+        "clientName": "twClient"
+    }).then(response => {
+        let data = response.data;
+    }).catch(err => {
+        console.log(err);
+    });
+
+    callback(null, 'Successfully executed');
 }
